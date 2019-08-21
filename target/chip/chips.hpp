550402170b643a4a7191ab9ff7a83d0e0ba0d069
Wiebe van Breukelen <wdownload@xs4all.nl>
2019-08-21 16:46:44 +0200

    Further configuration of cmake

--- target/chip/chips.hpp
+++
@@ -1,7 +0,0 @@
-// This file should be used by the linker to determine the correct target to use.
-
-#ifdef TARGET_CHIP_ARM_SAM3X8E
-#include "arm-sam3x8e/core.hpp"
-#endif
-
-#error "No target chip has been selected!"
